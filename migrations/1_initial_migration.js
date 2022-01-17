354eb374f8e4372f536b469a5d20589e3fcf62be
jimao <jimao@jimaodeMacBook-Pro.local>
2022-01-17 15:16:53 +0800

    部署

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,9 @@
 const Migrations = artifacts.require("Migrations");
 const Hello = artifacts.require("Hello");
+const Shabi = artifacts.require("Shabi");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
   deployer.deploy(Hello);
+  deployer.deploy(Shabi);
 };
